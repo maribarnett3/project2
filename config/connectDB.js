29d2db31329d292da7f96d340e2006ccf09d21e4
Kouros <kamousavi@gmail.com>
2020-05-15 10:58:49 -0400

    added CDN links for highlightJS

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,7 +3,7 @@ module.exports = {
             host: "localhost",
             port: 3306,
             user: "root",
-            password: "",
+            password: "poopoo12",
             database: "snippetsDB"
          }
 }
