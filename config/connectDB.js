9b37f90c4c7ffdbe2e69e12f0519d54b7b0dac85
Mariana Barnett <marianabarnett3@gmail.com>
2020-05-15 11:42:54 -0400

    node mysql seeder

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,9 +1,37 @@
+var connection = null;
+
+if (process.env.database) {
+    local = {
+    host: process.env.dbhost,
+
+    // Your port; if not 3306
+    port: process.env.dbport,
+
+    // Your username
+    user: process.env.dbuser,
+
+    // Your password
+    password: process.env.pw,
+    database: process.env.database
+  };
+} else {
+  var creds = require("../creds.json");
+
+  local = {
+    host: creds.host,
+
+    // Your port; if not 3306
+    port: creds.port,
+
+    // Your username
+    user: creds.user,
+
+    // Your password
+    password: creds.pw,
+    database: creds.database
+  };
+}
+
 module.exports = {
-    local : { 
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "rlatjdwls",
-            database: "snippetsDB"
-         }
+    local  
 }
