bcd362132479e8a6b1a067157ad1690299b64145
Mariana Barnett <marianabarnett3@gmail.com>
2020-05-16 23:49:06 -0400

    don't save empty value

--- server.js
+++ server.js
@@ -61,11 +61,11 @@ app.post("/api/updates/", function(req, res) {
     function (err, res) {
       if (err) throw err;
       console.log(res.affectedRows + " products updated!\n");
-  
+
     }
   );
-  
-  
+
+
   console.log(req.body)
 
   // connection.query("INSERT INTO plans (plan) VALUES (?)", [req.body.snippetData], function(err, result) {
@@ -78,28 +78,46 @@ app.post("/api/updates/", function(req, res) {
   //   console.log({ id: result.insertId });
   // });
 });
-
+function convertEmptyValuesToNull(req) {
+  for (const key in req.body) {
+    if (req.body.hasOwnProperty(key)) {
+      // if value is empty delete or set to null
+      if (!req.body[key])
+        req.body[key] = null
+    }
+  }
+  return req
+}
 
 app.post("/", (req, res) => {
-  connection.query(
-    "INSERT INTO snippets SET ?",
-    {
-      snippetTitle: req.body.newTitle,
-      language: req.body.newLanguage,
-      description: req.body.newDescription,
-      snippetBody: req.body.newSnippetBody
-    },
-    (err, res) =>{
-      if (err){
-        return res.status(500).end();
-      }
+  // don't save empty values, should be null
+  req = convertEmptyValuesToNull(req);
+  connection.query({
+    sql: `INSERT INTO snippets  
+    (snippetTitle, language, description, snippetBody) 
+    values(?,?,?,?)`,
+    values: [req.body.newTitle, req.body.newLanguage, req.body.newDescription, req.body.newSnippetBody]
+  },
+    (errSave, newdata) => {
+      if (errSave) {
+        console.log("error on submit")
+        console.log(errSave)
+        console.log(newdata)
+        connection.query("SELECT * FROM snippets", (err, data) => {
+          if (err) {
+            throw err;
+          }
+          // if there is an error, return error, and also request
+          res.render("index", { snippets: data, error: errSave, newSnippet: req.body });
+        });
+      } else
+      res.redirect("/");
     }
   );
-  res.redirect("/");
 });
 
-app.post("/api/:id", function(req, res){
-  connection.query("SELECT * FROM snippets WHERE id=" + req.params.id, function(err, data) {
+app.post("/api/:id", function (req, res) {
+  connection.query("SELECT * FROM snippets WHERE id=" + req.params.id, function (err, data) {
     if (err) throw err;
     // Log all results of the SELECT statement
     res.json(data);
